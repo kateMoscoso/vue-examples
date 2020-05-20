d4370931d2cb026e19cd04513947c4ce2a982d6a
kateMoscoso <katiemscs@gmail.com>
2020-05-20 19:53:21 +0200

    componentes externos

--- cli-exchange/src/views/CoinDetail.vue
+++ cli-exchange/src/views/CoinDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <template v-if="!isLoading">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -48,7 +51,9 @@
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          >Cambiar</button>
+          >
+            Cambiar
+          </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
@@ -63,6 +68,14 @@
           <span class="text-xl"></span>
         </div>
       </div>
+
+      <line-chart
+        class="my-10"
+        :colors="['orange']"
+        :min="min"
+        :max="max"
+        :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
+      />
     </template>
   </div>
 </template>
@@ -75,6 +88,7 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       asset: {},
       history: []
     }
@@ -107,13 +121,14 @@ export default {
   methods: {
     getCoin() {
       const id = this.$route.params.id
+      this.isLoading = true
 
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
+        .then(([asset, history]) => {
           this.asset = asset
           this.history = history
-        }
-      )
+        })
+        .finally(() => (this.isLoading = false))
     }
   }
 }
@@ -125,4 +140,3 @@ td {
   text-align: center;
 }
 </style>
-
