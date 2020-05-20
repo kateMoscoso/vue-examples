c7a2ae4b4d82385c0703061440a0c52d322593a2
kateMoscoso <katiemscs@gmail.com>
2020-05-20 18:29:09 +0200

    api and filters

--- cli-exchange/src/main.js
+++ cli-exchange/src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/main.css";
 import router from "@/router";
+import { dollarFilter, percentFilter } from '@/filters'
+
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+
 Vue.config.productionTip = false;
 
 new Vue({
